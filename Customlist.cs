bf314e9ecd637798d6efd060a700238fb21208c0
Nick Johnson <daddeus72@gmail.com>
2020-02-24 16:14:12 -0600

    End of workday 2/24 currently working on the add function

--- Customlist.cs
+++ Customlist.cs
@@ -6,11 +6,54 @@ using System.Threading.Tasks;
 
 namespace Customlistassng
 {
-   public class Customlist<T>
-    {
+    public class Customlist<T>
+    {//Membr Vars
+
+        int count = 0;
+        static int capacity = 4;
+
+        T[] list = new T[capacity];
+
+        //Constructor
+        public Customlist()
+        {
+
+        }
+            
+            
+           
+       
+
+
+       
 
 
 
+             
+
+
+
+        //methods
+
+
+        public void Add(T item) 
+        { int i = 0;
+            if (count>=(capacity+1))
+            {
+                T[] list = new T[capacity*2];
+            }
+
+            else { list[count] = item; count++; }
+        
+        
+        
+        
+        
+        
+        }
+
+
+       
 
 
     }
